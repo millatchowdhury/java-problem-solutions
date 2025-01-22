32a9bc2badf10519cb627435cfeefc9375504ba8
millatchowdhury <millatchowdhury@gmail.com>
2025-01-22 11:08:52 +0600

    Pattern Ok

---
+++ PatternFour.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PatternFour {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int row = n; row>=1; row--){
+            
+            
+            for(int col = 1; col<=n-row; col++){
+                System.out.print(" ");
+            }
+            for(int col = 1; col<=row; col++){
+                System.out.print(+col);
+            }
+            System.out.println();
+        }
+    }
+}
